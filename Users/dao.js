f757407caa030a1bdf448c4afd80030293e7be68
rocka852 <rock.kellen@gmail.com>
2024-06-17 14:06:12 -0400

    finish A6 3.6.2 Jun-17-24 2:06P

--- Users/dao.js
+++ Users/dao.js
@@ -3,8 +3,54 @@ import model from "./model.js"
 
 export const createUser = (user) => {}
 export const findAllUsers = () => model.find()
-export const finUserById = (userId)=> model.findById(userId)
+export const findUserById = (userId)=> model.findById(userId) // this is mongoose rule
 export const findUserByUsername = (username)=> model.findOne({username:username})
 export const findUserByCredentials = (username, password)=>model.findOne({username,password})
+export const findUsersByRole = (role)=> model.find({role:role})
 export const updateUser=(userId, user)=>model.updateOne({_id:userId},{$set:user})
 export const deleteUser=(userId)=>model.deleteOne({_id:userId})
+
+export const findUsersByPartialName = (partialName)=> {
+	const regex = new RegExp(partialName, "i") //i: case-insensitive 
+	return model.find({$or:[{firstName:{$regex:regex}}, {lastName:{$regex:regex}}]})
+}
+
+/* when you give http://localhost:4000/api/users/123 it will crash
+/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schema/objectId.js:250
+    throw new CastError('ObjectId', value, this.path, error, this);
+          ^
+
+CastError: Cast to ObjectId failed for value "123" (type string) at path "_id" for model "UserModel"
+    at SchemaObjectId.cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schema/objectId.js:250:11)
+    at SchemaType.applySetters (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schemaType.js:1236:12)
+    at SchemaType.castForQuery (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schemaType.js:1653:17)
+    at cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/cast.js:375:32)
+    at Query.cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:4798:12)
+    at Query._castConditions (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:2206:10)
+    at model.Query._findOne (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:2526:8)
+    at model.Query.exec (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:4342:80)
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at async findUserById (file:///Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/Users/routes.js:26:20) {
+  stringValue: '"123"',
+  messageFormat: undefined,
+  kind: 'ObjectId',
+  value: '123',
+  path: '_id',
+  reason: BSONError: input must be a 24 character hex string, 12 byte Uint8Array, or an integer
+      at new ObjectId (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/bson/lib/bson.cjs:2361:23)
+      at castObjectId (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/cast/objectid.js:25:12)
+      at SchemaObjectId.cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schema/objectId.js:248:12)
+      at SchemaType.applySetters (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schemaType.js:1236:12)
+      at SchemaType.castForQuery (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/schemaType.js:1653:17)
+      at cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/cast.js:375:32)
+      at Query.cast (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:4798:12)
+      at Query._castConditions (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:2206:10)
+      at model.Query._findOne (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:2526:8)
+      at model.Query.exec (/Users/imac/Documents/Kellen_Documents/Wyzant/Students/Adi_S_MA/Semester_E/My_git/kanbas-node-server-app/node_modules/mongoose/lib/query.js:4342:80),
+  valueType: 'string'
+}
+
+Node.js v20.12.2
+
+http://localhost:4000/api/users/666f7817c11886f8745f587e works 
+*/
--- Users/routes.js
+++ Users/routes.js
@@ -4,25 +4,52 @@ let currentUser = null
 export default function UserRoutes(app) {
 	const createUser = async(req,res) =>{}
 	const deleteUser = async (req, res) => { };
-  	const findAllUsers = async (req, res) => { 
-        const users = await dao.findAllUsers()
+  const findAllUsers = async (req, res) => { 
+    const {role, name} = req.query
+    //console.log("role is: " + role)
+      if (role) {
+
+        const users = await dao.findUsersByRole(role);
+          //console.log(users)
         res.json(users)
-    };
-  	const findUserById = async (req, res) => { };
-  	const updateUser = async (req, res) => { };
-  	const signup = async (req, res) => { };
-  	const signin = async (req, res) => { };
-  	const signout = (req, res) => { };
-  	const profile = async (req, res) => { };
-
-  	app.post("/api/users", createUser)
-  	app.get("/api/users", findAllUsers)
-  	app.get("/api/users/:userId", findUserById)
-  	app.put("/api/users/:userId", updateUser)
-  	app.delete("/api/users/:userId", deleteUser)
-
-  	app.post("/api/users/signup", signup)
-  	app.post("/api/users/signin", signin)
-  	app.post("/api/users/signout", signout)
-  	app.post("/api/users/profile", profile)
+        return
+      }  
+      if (name) {
+        const users = await dao.findUsersByPartialName(name)
+        res.json(users)
+        return
+      }      
+      const users = await dao.findAllUsers()
+      res.json(users)
+  };
+  const findUserById = async (req, res) => { 
+      console.log("req.params is: " + JSON.stringify(req.params))
+      console.log("req.params.userId is: " + req.params.userId)
+      //req.params is: {"userId":"666f7817c11886f8745f587e"}
+      //req.params.userId is: 666f7817c11886f8745f587e
+      /*
+      if (req.params.userId.length == 3){
+        console.log(req.params.userId + "length 3")
+        let temp = parseInt(req.params.userId)//without let this won't work
+        const user = await dao.findUserById(temp)
+        res.json(user)
+        return
+      }*/
+      const user = await dao.findUserById(req.params.userId)
+      res.json(user)
+  };
+  const updateUser = async (req, res) => { };
+  const signup = async (req, res) => { };
+  const signin = async (req, res) => { };
+  const signout = (req, res) => { };
+  const profile = async (req, res) => { };
+  app.post("/api/users", createUser)
+  app.get("/api/users", findAllUsers)
+  app.get("/api/users/:userId", findUserById)
+  app.put("/api/users/:userId", updateUser)
+  app.delete("/api/users/:userId", deleteUser)
+  app.post("/api/users/signup", signup)
+  app.post("/api/users/signin", signin)
+  app.post("/api/users/signout", signout)
+  app.post("/api/users/profile", profile)
 }
