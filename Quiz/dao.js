3e6804ec28e7fcaae94b311e099b2af71716dbca
rocka852 <rock.kellen@gmail.com>
2024-06-23 21:54:21 -0400

    findQuiz by quizId 1

--- Quiz/dao.js
+++ Quiz/dao.js
@@ -2,5 +2,5 @@ import model from "./model.js"
 
 export const createQuiz = (quiz) => {}
 export const findAllQuiz = () => model.find()
-export const findQuizByCourse = (courseId) => model.findById(quizeId)
+export const findQuizById = (quizId) => model.find({"qid":quizId})
 export const updateQuiz = (quizId, quiz) => model.updateOne({_id: quizId}, {$set: quiz})
--- Quiz/routes.js
+++ Quiz/routes.js
@@ -9,8 +9,10 @@ export default function QuizRoutes(app) {
 
 	}
 
-	const findQuizByCourse = async(req, res) => {
-
+	const findQuizById = async(req, res) => {
+		console.log(JSON.stringify(req.params))
+		const quiz = await dao.findQuizById(req.params.quizId)
+		res.json(quiz)
 	}
 
 	const updateQuiz = async(req, res) => {
@@ -19,6 +21,6 @@ export default function QuizRoutes(app) {
 
 	app.post("/api/quizes", createQuiz)
 	app.get("/api/quizes", findAllQuiz)
-	app.get("/api/quizes/:quizId", findQuizByCourse)
+	app.get("/api/quizes/:quizId", findQuizById)
 	app.put("/api/quizes/:quizId", updateQuiz)
 }
