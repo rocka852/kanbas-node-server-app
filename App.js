dea76103059a3dbf27492ed300dd42907557ec88
rocka852 <rock.kellen@gmail.com>
2024-06-29 03:34:34 -0400

    add hellokitty

--- App.js
+++ App.js
@@ -35,7 +35,8 @@ const sessionOptions = {
   	resave: false,
   	saveUninitialized: false,
 }
-if (process.env.NODE_ENV !== "development") {
+//if (process.env.NODE_ENV !== "development") {
+if (process.env.NODE_ENV === "hellokitty") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
